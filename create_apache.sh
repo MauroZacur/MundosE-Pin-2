dce00de5dac9fc3326711ffe66ea306cf6221d48
Mauro <maurogzacur@gmail.com>
2024-09-18 02:15:32 -0300

    despliegue

--- create_apache.sh
+++ create_apache.sh
@@ -1,4 +1,13 @@
-#! /bin/bash
+#!/bin/bash
+# Actualizar el sistema y asegurar los paquetes
 sudo yum update -y
-sudo yum install -y httpd.x86_64
-sudo systemctl enable httpd --now
+
+# Instalar Apache
+sudo yum install -y httpd
+
+# Iniciar y habilitar Apache para que se inicie en el arranque
+sudo systemctl start httpd
+sudo systemctl enable httpd
+
+# Crear una página de inicio sencilla
+echo "<h1>Bienvenido a apache</h1>" | sudo tee /var/www/html/index.html
--- setup.tf
+++ setup.tf
@@ -1,31 +1,9 @@
-provider "aws" {
-  region = "us-east-1"
+# Crear la tabla de rutas por defecto para permitir tráfico a Internet
+resource "aws_internet_gateway" "my_igw" {
+  vpc_id = aws_vpc.vpc.id  # Cambiado a aws_vpc.vpc.id
 }
 
-
-
-#Get Linux AMI ID using SSM Parameter endpoint in us-east-1
-#data "aws_ssm_parameter" "webserver-ami" {
-#  name = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
-#}
-
-#Create VPC in us-east-1
-resource "aws_vpc" "vpc" {
-  cidr_block           = "10.0.0.0/16"
-  enable_dns_support   = true
-  enable_dns_hostnames = true
-  tags = {
-    Name = "terraform-vpc"
-  }
-
-}
-
-#Create IGW in us-east-1
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.vpc.id
-}
-
-#Get main route table to modify
+# Obtener la tabla de rutas principal de la VPC
 data "aws_route_table" "main_route_table" {
   filter {
     name   = "association.main"
@@ -33,39 +11,28 @@ data "aws_route_table" "main_route_table" {
   }
   filter {
     name   = "vpc-id"
-    values = [aws_vpc.vpc.id]
+    values = [aws_vpc.vpc.id]  # Cambiado a aws_vpc.vpc.id
   }
 }
-#Create route table in us-east-1
+
+# Crear la tabla de rutas por defecto para permitir tráfico a Internet
 resource "aws_default_route_table" "internet_route" {
   default_route_table_id = data.aws_route_table.main_route_table.id
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
+    gateway_id = aws_internet_gateway.my_igw.id
   }
   tags = {
     Name = "Terraform-RouteTable"
   }
 }
 
-#Get all available AZ's in VPC for master region
-data "aws_availability_zones" "azs" {
-  state = "available"
-}
+# Crear un grupo de seguridad para permitir tráfico en TCP/80 (HTTP) y TCP/22 (SSH)
+resource "aws_security_group" "my_sg" {
+  name        = "web_sg"
+  description = "Allow HTTP and SSH traffic"
+  vpc_id      = aws_vpc.vpc.id  # Cambiado a aws_vpc.vpc.id
 
-#Create subnet # 1 in us-east-1
-resource "aws_subnet" "subnet" {
-  availability_zone = element(data.aws_availability_zones.azs.names, 0)
-  vpc_id            = aws_vpc.vpc.id
-  cidr_block        = "10.0.1.0/24"
-}
-
-
-#Create SG for allowing TCP/80 & TCP/22
-resource "aws_security_group" "sg" {
-  name        = "sg"
-  description = "Allow TCP/80 & TCP/22"
-  vpc_id      = aws_vpc.vpc.id
   ingress {
     description = "Allow SSH traffic"
     from_port   = 22
@@ -73,21 +40,19 @@ resource "aws_security_group" "sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
   ingress {
-    description = "allow traffic from TCP/80"
+    description = "Allow HTTP traffic"
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-}
-
-output "Webserver-Public-IP" {
-  value = aws_instance.webserver.public_ip
 }
